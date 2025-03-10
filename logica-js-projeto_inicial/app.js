97fd2676ec8d37ad629d20e08f36528b23e685c3
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-10 19:13:49 -0300

    Adição do else na estrtura do if

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,12 +3,17 @@ alert('Boa vindas ao jogo do número secreto');
 
 /*let é a variável utilizada no JS aonde atribuimmos um nome e um valor a essa var*/
 /*prompt serve para podermos interagir com usuário fazendo ele escrever*/
-let numeroSecreto = 29;
+let numeroSecreto = 15;
 let chute = prompt('Escolha um número entre 1 e 30');
+console.log(numeroSecreto)
 
-/*if e else é um método de comparação, serve para comparar condições e 
-valores e ver se está correto*/
+//if e else é um método de comparação, serve para comparar condições e 
+//valores e ver se está correto
+// console serve para o desenvolvedor acompanhar o código e ver se está tudo nos conformes
+// para concatenar valores, utilizamos a crase e para mostrar o valor da var usamos o ${}
 
 if (numeroSecreto == chute) {
-   console.log('Você descobriu o número secreto');
+   alert('Você descobriu o número secreto' + numeroSecreto);
+} else {
+   alert('você errou!')
 }
