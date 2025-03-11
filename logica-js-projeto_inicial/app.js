00f4538f071ea25bb8c2ff42472b49cbe5ac406a
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-11 13:39:18 -0300

    Laço de repetição com while feito

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,16 +4,25 @@ alert('Boa vindas ao jogo do número secreto');
 /*let é a variável utilizada no JS aonde atribuimmos um nome e um valor a essa var*/
 /*prompt serve para podermos interagir com usuário fazendo ele escrever*/
 let numeroSecreto = 15;
-let chute = prompt('Escolha um número entre 1 e 30');
-console.log(numeroSecreto)
+let chute;
+console.log(numeroSecreto);
 
 //if e else é um método de comparação, serve para comparar condições e 
 //valores e ver se está correto
 // console serve para o desenvolvedor acompanhar o código e ver se está tudo nos conformes
 // para concatenar valores, utilizamos a crase e para mostrar o valor da var usamos o ${}
 
-if (numeroSecreto == chute) {
-   alert('Você descobriu o número secreto' + numeroSecreto);
-} else {
-   alert('você errou!')
+while (chute != numeroSecreto) {
+   chute = prompt('Escolha um número entre 1 e 30');
+
+   if (chute == numeroSecreto) {
+      alert('Você descobriu o número secreto' + numeroSecreto);
+   } else {
+      if (chute > numeroSecreto) {
+         alert('O número secreto é menor que' + chute);
+      } else {
+         alert('O número secreto é maior que' + chute);
+      }
+   }
 }
+
